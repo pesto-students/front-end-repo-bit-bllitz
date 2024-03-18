d0b91669924f9de07ee56785ba706d0f9a322473
Suvigya-Sankalp-595 <suvigya.tiwary6568@gmail.com>
2024-03-19 03:48:25 +0530

    renovate orders api with redux and cart

---
+++ point-of-sales/app/api/orders/generateOrder.js
@@ -0,0 +1,39 @@
+import { supabase } from "../../../supabase/supabase"
+import { generateOrder } from "@/lib/redux/slices/orderSlice";
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+      try {
+        // Fetch orders from the "orders" table
+        const { data, error } = await supabase.from('orders').select('*');
+  
+        if (error) {
+          throw error;
+        }
+  
+        // Return the fetched orders
+        res.status(200).json(data);
+      } catch (error) {
+        console.error('Error fetching orders:', error.message);
+        res.status(500).json({ error: 'Error fetching orders' });
+      }
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' });
+    }
+    if (req.method === 'POST') { 
+     // Assuming customerId and cartItems are passed in the request body
+      // Assuming customerId and cartItems are passed in the request body
+     try {
+      const cartItems = selectCartItems(store.getState());
+       // Dispatch action to generate order
+       await store.dispatch(generateOrder({ customerId, cartItems }));
+       res.status(200).json({ message: 'Order generated successfully' });
+     } catch (error) {
+       console.error('Error generating order:', error);
+       res.status(500).json({ error: 'Internal Server Error' });
+     }
+   } else {
+     res.setHeader('Allow', ['POST']);
+     res.status(405).end(`Method ${req.method} Not Allowed`);
+   }
+}
+
