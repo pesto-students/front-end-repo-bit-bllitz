f0dffd96ad9550eedb5c3ae3b4796d104dc5b7c4
Zeenat <zeenatkm301@gmail.com>
2024-04-08 21:18:22 +0530

    reset password logic implemented with confirm password pages

---
+++ point-of-sales/app/auth/confirm/page.js
@@ -0,0 +1,70 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import styles from "./ConfirmPassword.module.scss";
+import CustomInput from "@/components/auth/input/CustomInput";
+import CustomButton from "@/components/button/CustomButton";
+import { supabase } from "@/supabase/supabase";
+import { useAppContext } from "@/context";
+import { useRouter } from "next/navigation";
+const ConfirmPassword = () => {
+  const { setUser } = useAppContext();
+  const router = useRouter();
+  const [data, setData] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+  useEffect(() => {
+    const fetchSession = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      console.log("dashboard data", data);
+      if (data.session == null) {
+        router.push("/auth/signin");
+      } else {
+        setUser(data);
+      }
+    };
+    fetchSession();
+  }, []);
+
+  const handleConfirm = async () => {
+    const { password, confirmPassword } = data;
+    // if (password !== confirmPassword) {
+    //   alert("Passwords do not match!");
+    // }
+    if (password == confirmPassword) {
+      const { data: userData, error } = await supabase.auth.getUser();
+      console.log("userData", userData.user);
+      try {
+        const { user = {} } = userData;
+        const { data: resetData, error } = await supabase.auth.updateUser({
+          password: data.password,
+        });
+        console.log("reset data", resetData);
+      } catch (error) {
+        console.log("error in confirm password", error);
+      }
+    }
+  };
+  return (
+    <div>
+      <label>Enter your password</label>
+      <CustomInput
+        type={"password"}
+        value={data.password}
+        placeholder={"Enter Password"}
+        onChange={(e) => setData({ password: e.target.value })}
+      />
+      <label>Confirm your password</label>
+
+      <CustomInput
+        type={"password"}
+        value={data.confirmPassword}
+        placeholder={"Confirm Password"}
+        onChange={(e) => setData({ confirmPassword: e.target.value })}
+      />
+      <CustomButton text={"Reset Password"} onClick={handleConfirm} />
+    </div>
+  );
+};
+
+export default ConfirmPassword;
--- point-of-sales/components/profile/UserLoginData.js
+++ point-of-sales/components/profile/UserLoginData.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../app/profile/profile.module.scss";
-import { Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Modal, Typography } from "@mui/material";
 import CustomInput from "../auth/input/CustomInput";
 import KeyIcon from "@mui/icons-material/Key";
 import CustomButton from "../button/CustomButton";
 import { supabase } from "../../supabase/supabase";
+import { useRouter } from "next/navigation";
 const UserLoginData = ({ setFormData, formData }) => {
+  const router = useRouter();
+  const [resetPassword, setResetPassword] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [modal, setModal] = useState(false);
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     console.log(name, value, "nameval");
@@ -14,13 +20,43 @@ const UserLoginData = ({ setFormData, formData }) => {
       [name]: value,
     });
   };
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    boxShadow: 24,
+    p: 4,
+  };
+  const handleReset = async () => {
+    const { data: userData, error } = await supabase.auth.getUser();
+    console.log("userData", userData.user);
+    try {
+      const { user = {} } = userData;
+      const { data, error } = await supabase.auth.resetPasswordForEmail(
+        user.email,
+        {
+          redirectTo: `${location.origin}/confirm`,
+        }
+      );
+      console.log("resetData", data);
+      setSuccess(true);
+    } catch (error) {}
+    setModal(false);
+  };
   return (
     <div>
       <Typography variant="h5" className={styles.infoCopy}>
         Login & Password
       </Typography>
       <Grid container className={styles.loginForm}>
-        <Grid item md={5.8} sm={5}>
+        <Grid item md={12} sm={5}>
           <div className={styles.label}>Username</div>
           <CustomInput
             placeholder={"email"}
@@ -30,8 +66,8 @@ const UserLoginData = ({ setFormData, formData }) => {
             value={formData.email}
           />
         </Grid>
-        <Grid item md={0.4} sm={1} />
-        <Grid item md={5.8} sm={5}>
+        {/* <Grid item md={0.4} sm={1} /> */}
+        {/* <Grid item md={5.8} sm={5}>
           <div className={styles.label}>Your sales ID number</div>
           <CustomInput
             placeholder={"sales-id"}
@@ -40,7 +76,7 @@ const UserLoginData = ({ setFormData, formData }) => {
             height={"2rem"}
             disabled={true}
           />
-        </Grid>
+        </Grid> */}
         {/* <Grid item md={5.8} sm={5}>
           <div className={styles.label}>Password</div>
           <CustomInput
@@ -51,13 +87,41 @@ const UserLoginData = ({ setFormData, formData }) => {
           />
         </Grid>
         <Grid item md={0.4} />*/}
-        <Grid item md={5.8} sm={5} className={styles.changePass}>
-          <Typography variant="body2" className={styles.copy} >
+        <Grid item md={12} sm={5} className={styles.changePass}>
+          <Typography
+            variant="body2"
+            className={styles.copy}
+            onClick={() => setModal(true)}
+          >
             <KeyIcon /> Change Password
           </Typography>
-        </Grid> 
+          {success && (
+            <div className={styles.password}>
+              <span>Success! </span>Reset link has been sent to your registered
+              e-mail address
+            </div>
+          )}
+        </Grid>
       </Grid>
       <CustomButton text={"Save changes"} />
+      <Modal
+        open={modal}
+        onClose={() => setModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className={styles.modalContainer}
+      >
+        <Box sx={style}>
+          <Typography className={styles.head}>Forgot your Password?</Typography>
+          <Typography className={styles.copy}>
+            You'll get a link on your registered E-mail address{" "}
+          </Typography>
+          <CustomButton
+            text="Reset Password"
+            onClick={() => handleReset()}
+          ></CustomButton>
+        </Box>
+      </Modal>
     </div>
   );
 };
